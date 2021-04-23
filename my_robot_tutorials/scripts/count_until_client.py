9564d1fbc10f550947e9ee03dda308b1b5099796
Drojas251 <52464724+Drojas251@users.noreply.github.com>
2021-04-23 11:16:26 -0700

    updated code

--- my_robot_tutorials/scripts/count_until_client.py
+++ my_robot_tutorials/scripts/count_until_client.py
@@ -12,20 +12,30 @@ class CountUntilClient:
         self._ac = actionlib.SimpleActionClient('/count_until',CountUntilAction) # initialize client ' same topic '
         self._ac.wait_for_server() # wait for server to be up ** important 
         rospy.loginfo("action server is up")
+
     def send_goal_and_get_results(self):
-        goal = CountUntilGoal(max_number = 10, wait_duration = 0.5)
+        goal = CountUntilGoal(max_number = 12, wait_duration = 0.5)
         # how to send the goal?
-        self._ac.send_goal(goal)
+        # use a call back to trigger the goal. Needed to make action asynchronous
+        self._ac.send_goal(goal, done_cb= self.done_callback, feedback_cb = self.feedback_callback)
         rospy.loginfo("goal has been sent")
-        success = self._ac.wait_for_result(rospy.Duration(3.0)) #  returns a boolean if results take longer than 3s
-        if not success:
-            rospy.loginfo("TIMEOUT")
         # now we need to get result
-        self._ac.wait_for_result()
-        self._ac.get_result()
-        rospy.loginfo(self._ac.get_result())
+        #self._ac.wait_for_result()
+        #self._ac.get_result()
+        #rospy.loginfo(self._ac.get_result())
+
+	rospy.sleep(2)
+	self._ac.cancel_goal() # method to request the serve to cancel a goal
+
+    def done_callback(self,status,result):
+        rospy.loginfo("Status is: " + str(status))
+        rospy.loginfo("Result is: " + str(result))
+
+    def feedback_callback(self,feedback):
+	rospy.loginfo(feedback)
 
 if __name__ == '__main__':
     rospy.init_node('count_until_client')
     client = CountUntilClient()
     client.send_goal_and_get_results()
+    rospy.spin()
--- my_robot_tutorials/scripts/count_until_server.py
+++ my_robot_tutorials/scripts/count_until_server.py
@@ -6,6 +6,7 @@ import actionlib
 from my_robot_msgs.msg import CountUntilAction  # for actions you just need to import the messge ' my_robot_msgs.msg'
 from my_robot_msgs.msg import CountUntilGoal
 from my_robot_msgs.msg import CountUntilResult
+from my_robot_msgs.msg import CountUntilFeedback
 
 class CountUntilServer:
 	
@@ -21,22 +22,53 @@ class CountUntilServer:
 	def get_goal(self,goal):
     		rospy.loginfo("A goal has been recieve")
 		rospy.loginfo(goal)
-
+		# action message variables
 		max_number = goal.max_number
 		wait_duration = goal.wait_duration
 
 		self._counter = 0
 		rate = rospy.Rate(1.0/wait_duration)
+		
+		success = False
+		preempted = False
 
-		while self._counter < max_number:
+		while not rospy.is_shutdown():
     			self._counter += 1
+
+			if self._as.is_preempt_requested():
+				# condition to cancel a goal
+				# is_preemt_request() returns a boolean
+				preempted = True
+				break
+			if self._counter > 9:
+				# add this condition as a failure. 
+				# if this action does not work properly, it stops
+				break
+			if self._counter >= max_number:
+				success = True
+				break
 			rospy.loginfo(self._counter)
+			# How to publish feedback
+			feedback = CountUntilFeedback()
+			feedback.percentage = float(self._counter)/float(max_number)
+			self._as.publish_feedback(feedback)
 			rate.sleep()
+
+
+
 		result = CountUntilResult()
 		result.count = self._counter
 		# how to send the result ?
-
-		self._as.set_succeeded(result)
+		if preempted:
+			rospy.loginfo("preempted")
+			# changes status 
+			self._as.set_preempted(result)
+		elif success:
+			rospy.loginfo("success")
+			self._as.set_succeeded(result)
+		else:
+			rospy.loginfo("failure")
+			self._as.set_aborted(result) # aborts the action 
 
 if __name__ == '__main__':
     	rospy.init_node('count_until_server')
